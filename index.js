391629fb1aa5558a4afb33e45b61c9e55113e74f
jpalvesgoulart <jpalvesgoulart@outlook.com>
2023-03-04 10:48:12 -0300

    Criando README e finalizando código js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/* Chegou a hora, João Pedro Alves Goulart! Você vai começar a sua jornada no #7DaysOfCode ;)
-
-Este primeiro dia é super importante! Ao final dele, você terá um novo conhecimento que é essencial para os próximos desafios.
+/* Este primeiro dia é super importante! Ao final dele, você terá um novo conhecimento que é essencial para os próximos desafios.
 
 Existe uma situação super comum para quem utiliza o Javascript: problemas com os tipos de variáveis na hora de comparar os valores de duas variáveis entre si. Eu já passei muito por isso!
 
@@ -23,46 +21,34 @@ O que não faz necessariamente muito sentido.
 
 Sendo assim, a sua tarefa de hoje é reescrever o código abaixo de maneira que ele imprima as informações de maneira correta, que faça sentido e sem erros: */
 
-let numeroUm = 1
-let stringUm = '1'
-let numeroTrinta = 30
-let stringTrinta = '30'
-let numeroDez = 10
-let stringDez = '10'
+let numero1 = 1
+let string1 = '1'
+let numero2 = 30
+let string2 = '30'
+let numero3 = 10
+let string3 = '10'
 
-if (numeroUm == stringUm && numeroUm !== stringUm) {
-  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
-}   else if (numeroUm === stringUm) {
-        console.log('As variáveis numeroUm e stringUm tem o mesmo valor e tipos iguais')   
+if (numero1 == string1 && numero1 !== string1) {
+  console.log('Os valores ', numero1, ' e ',  string1, ' têm o mesmo valor, mas tipos diferentes.')
+}   else if (numero1 === string1) {
+        console.log('Os valores ', numero1, ' e ', string1, ' têm o mesmo valor e tipos iguais.')   
 }   else {
-        console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+        console.log('Os valores ', numero1, ' e ', string1, ' não têm o mesmo valor.')
 }
 
-if (numeroUm == stringUm && numeroUm !== stringUm) {
-    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
-}   else if (numeroUm === stringUm) {
-          console.log('As variáveis numeroUm e stringUm tem o mesmo valor e tipos iguais')   
+if (numero2 == string2 && numero2 !== string2) {
+  console.log('Os valores ', numero2, ' e ',  string2, ' têm o mesmo valor, mas tipos diferentes.')
+}   else if (numero2 === string2) {
+        console.log('Os valores ', numero2, ' e ', string2, ' têm o mesmo valor e tipos iguais.')   
 }   else {
-          console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+        console.log('Os valores ', numero2, ' e ', string2, ' não têm o mesmo valor.')
 }
 
-if (numeroUm == stringUm && numeroUm !== stringUm) {
-  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
-}   else if (numeroUm === stringUm) {
-        console.log('As variáveis numeroUm e stringUm tem o mesmo valor e tipos iguais')   
+if (numero3 == string3 && numero3 !== string3) {
+  console.log('Os valores ', numero3, ' e ',  string3, ' têm o mesmo valor, mas tipos diferentes.')
+}   else if (numero3 === string3) {
+        console.log('Os valores ', numero3, ' e ', string3, ' têm o mesmo valor e tipos iguais.')   
 }   else {
-        console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+        console.log('Os valores ', numero3, ' e ', string3, ' não têm o mesmo valor.')
 }
 
-/* if (COMPARAR O numeroTrinta e a stringTrinta) {
-  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
-} else {
-  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
-}
-
-if (COMPARAR O numeroDez e a stringDez) {
-  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
-} else {
-  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
-}
- */
